10cff51712fdb151e1135a29a3a07d2b7be0a979
iamgreatofweb3 <iamgreat045@gmail.com>
2026-06-20 22:47:01 +0100

    added metadata at the layout.jsx section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,21 @@
+import { title } from "framer-motion/client";
 import "./globals.css";
+import { URLPattern } from "next/server";
 export const metadata = {
   title: "Welcome to Bixie-Car World",
-  description: "Car Renting Website Build by GreatOF web3",
+  description: "Car Renting Website Build by GreatOfWeb3",
+  openGraph:{
+    title:"Welcome to Bixxie-Car-World",
+    description:"Affordable and reliable Car renting services",
+    images:[
+      {
+        url :"/car1.png",
+        width:1200,
+        height:630,
+        alt :"Bixxie Car Rental"
+      }
+    ]
+  }
 };
 
 export default function RootLayout({ children }) {
