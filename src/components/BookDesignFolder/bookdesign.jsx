9524edf1df370ecacf42a118d3eab781a75c3d35
iamgreatofweb3 <iamgreat045@gmail.com>
2026-06-24 06:33:32 +0100

    still designing

--- src/components/BookDesignFolder/bookdesign.jsx
+++ src/components/BookDesignFolder/bookdesign.jsx
@@ -2,6 +2,7 @@ import "./bookdesign.css"
 import Link from "next/link"
 import Image from "next/image"
 import { FaAdjust, FaForward } from "react-icons/fa"
+import {ChevronsRightIcon} from "lucide-react"
 import { FaDiagramNext, FaFaceGrimace } from "react-icons/fa6"
 export default function BookDesignCnt() {
     return(
@@ -13,11 +14,11 @@ export default function BookDesignCnt() {
                     <h5>Book your Perfect ride and  
                     enjoy with Bixxie Ridez rental services</h5>
                      <button>
-                                <Link href="booking" className="Links">BOOK NOW <FaAdjust/></Link>
+                                <Link href="booking" className="Links">BOOK NOW <ChevronsRightIcon height={30} width={30}/></Link>
                             </button>
                 </div>
                 <div className="right-part">
-                    <Image src="/car-bg-removed.png" width={300} height={200} alt="SlideImg" className="slideImg"/>
+                    <Image src="/car-bg-removed.png" width={450} height={300} alt="SlideImg" className="slideImg"/>
                 </div>
             </div>
 
