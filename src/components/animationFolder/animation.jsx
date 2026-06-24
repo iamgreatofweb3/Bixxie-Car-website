6716251dfa06134cc025b2dea2acec154cc76ac7
iamgreatofweb3 <iamgreat045@gmail.com>
2026-06-24 07:25:17 +0100

    redesign ongoing

--- src/components/animationFolder/animation.jsx
+++ src/components/animationFolder/animation.jsx
@@ -22,7 +22,7 @@ export default function AnimationArea() {
                  <div className="grid-box">
                         <div className="first-grid">
                             {/* <li> <Award stroke="crimson" strokeWidth={1} width={40} height={40} className="iconAnimate"/> Professional and well trained chauffeurs</li> */}
-                            <li> <Award color="crimson"   className="iconAnimate " style={{width:"40px",height:"40px"}}/> Professional and well trained chauffeurs</li>
+                            <li> <Award color="crimson"   className="iconAnimate " style={{width:"40px",height:"40px"}}/> Professional  chauffeurs</li>
                              <li><FaTags color="crimson"  width={80} height={140} className="iconAnimate " style={{width:"40px",height:"40px"}}/> Excellent Services</li>
                             <li><FaClockRotateLeft color="crimson"className="iconAnimate " style={{width:"40px",height:"40px"}}/>Pucntuality</li>
                         </div>
